b2260632cf3c6e05adad6bc1f3c850cde027a538
Trevor West <trevorwest@duck.com>
2024-02-23 10:39:18 -0600

    Build working repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if(num < 0) {
+    return "ERROR";
+  } else {
+    let loopedString = "";
+    for (let i = 0; i < num; i++) {
+      loopedString += string;
+    }
+    return loopedString;
+  }
 };
 
 // Do not edit below this line
