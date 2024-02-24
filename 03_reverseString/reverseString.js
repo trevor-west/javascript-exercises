4f978c1dbccb8b33c1e39a24fbeeec5fb8355cc0
Trevor West <trevorwest@duck.com>
2024-02-24 09:46:58 -0600

    Finish loop and return reversed Str

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,8 +2,10 @@ const reverseString = function(string) {
   let reversedStr = "";
 
   for (let i = string.length; i > 0; i--){
-    
+    reversedStr += string[i];
   }
+
+  return reversedStr;
 };
 
 // Do not edit below this line
