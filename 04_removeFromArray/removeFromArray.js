acb170f7143235c4440e5054f66b248910fc8ca7
Trevor West <trevorwest@duck.com>
2024-02-25 10:10:03 -0600

    Remove debugging console.logs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,9 @@
 const removeFromArray = function(array, ...num) {
-  console.log(`num before call: ${num}`);
-  console.log(`...num before call: `);
-  console.log(...num);
-  let numArray = ...num;
-  console.log(`numArray: ${numArray}`);
+
   for (let i = 0; i < array.length; i++){
-    console.log(`num before first for call: ${num}`);
-    console.log(`...num before first for call: `);
-    console.log(...num);
-    if(array[i] == num) {
-      console.log(`i: ${i}`);
-      console.log(`array[i]:  ${array[i]}`);
-      return array.slice(num - 1, num);
+    if(array[i] == num[i]) {
+      array.slice(num[i] - 1, num[i]);
+      console.log(array);
     }
   }
   
