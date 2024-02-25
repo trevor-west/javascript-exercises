d042193852a777093393833f4785c40c4cf97571
Trevor West <trevorwest@duck.com>
2024-02-24 18:25:10 -0600

    Make i >= 0 to fix loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function(string) {
   let reversedStr = "";
 
-  for (let i = string.length - 1; i > 0; --i){
+  for (let i = string.length - 1; i >= 0; --i){
     reversedStr += string[i];
     console.log(i);
     console.log(reversedStr);
