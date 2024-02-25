902e63e6352893c9b2db9f39f8933d0f4a452a77
Trevor West <trevorwest@duck.com>
2024-02-25 09:56:12 -0600

    Added console.log for debugging

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
 const removeFromArray = function(array, ...num) {
   for (let i = 0; i < num; i++){
     if(array[i] == num) {
+      console.log(`i: ${i}`);
+      console.log(`array[i]:  ${array[i]}`);
       return array.slice(num - 1, num);
     }
   }
