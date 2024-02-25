b016faed6b11a4cc3d32b0eaa207ceedb523b5ff
Trevor West <trevorwest@duck.com>
2024-02-24 19:30:18 -0600

    Add removedArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function(array, num, numTwo) {
-  return array[num - 1] && array[numTwo -1];
+const removeFromArray = function(array, num, ...moreNum) {
+  let removedArray = [];
+  removedArray.push(array[num - 1], array[...moreNum - 1]);
+
+  return removedArray;
 };
 
 // Do not edit below this line
