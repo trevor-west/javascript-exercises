af832d8a073f235820319f81f593a30c5323b25d
Trevor West <trevorwest@duck.com>
2024-02-25 09:41:33 -0600

    Add for loop with conditional check plus slice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,12 @@
 const removeFromArray = function(array, ...num) {
-  let removedArray = [];
-  for (num of array)  {
-    removedArray = array.pop(num - 1);
-  };
-
-  return removedArray;
-};
+  for (let i = 0; i < num; i++){
+    if(array[i] == num) {
+      return array.slice(num - 1, num);
+    }
+  }
+  
+  return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
