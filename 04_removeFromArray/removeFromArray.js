6b388264c5633539e0a0fc897e08ab166868490f
Trevor West <trevorwest@duck.com>
2024-02-25 10:02:08 -0600

    Loop is fixed, add numArray to house ...num

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,13 @@
 const removeFromArray = function(array, ...num) {
   console.log(`num before call: ${num}`);
-  console.log(`...num before call: ${...num}`);
-  for (let i = 0; i < num; i++){
+  console.log(`...num before call: `);
+  console.log(...num);
+  let numArray = ...num;
+  console.log(`numArray: ${numArray}`);
+  for (let i = 0; i < array.length; i++){
+    console.log(`num before first for call: ${num}`);
+    console.log(`...num before first for call: `);
+    console.log(...num);
     if(array[i] == num) {
       console.log(`i: ${i}`);
       console.log(`array[i]:  ${array[i]}`);
