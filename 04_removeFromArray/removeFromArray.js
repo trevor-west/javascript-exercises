0f2670672732a5dfaf755fc6da93b95467910663
Trevor West <trevorwest@duck.com>
2024-02-28 12:30:59 -0600

    Add needed } to close for loop and function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -5,9 +5,10 @@ const removeFromArray = function(array, ...num) {
   for (let i = 0; i < array.length; i++){
     if(array[i] === num[i]) {
       array.splice(num, 1);
+    }
   }
-
   return array;
+
 }
 
 // Do not edit below this line
