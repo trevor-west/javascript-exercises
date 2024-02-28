2fb125c9e67a496232252c371cdce0c237d1e58f
Trevor West <trevorwest@duck.com>
2024-02-28 12:29:11 -0600

    Condense code and change array.splice
    Get rid of unused code and change
    array.splice(num, num.length) to
    array.splice(num, 1) as each loop will only
    remove one array item at a time. Otherwise,
    each loop would just snip more than it should
    off array in each loop pass.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,11 +3,8 @@ const removeFromArray = function(array, ...num) {
 // if num is 1, 2, 3, then num = [1, 2, 3]
 
   for (let i = 0; i < array.length; i++){
-    if(array[i] === num) {
-      array.splice(num - 1, 1);
-    } else if (array[i] === moreNum[i]) {
-      array.splice(...moreNum() - 1, num.length);
-    }
+    if(array[i] === num[i]) {
+      array.splice(num, 1);
   }
 
   return array;
