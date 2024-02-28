25c7a73ed6159e68327d088407113783cd666a4a
Trevor West <trevorwest@duck.com>
2024-02-28 12:28:08 -0600

    Change 1 to num.length to account for full array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,12 @@
 const removeFromArray = function(array, ...num) {
+// if num is 1, num = [1]
+// if num is 1, 2, 3, then num = [1, 2, 3]
 
   for (let i = 0; i < array.length; i++){
     if(array[i] === num) {
       array.splice(num - 1, 1);
     } else if (array[i] === moreNum[i]) {
-      array.splice(...moreNum() - 1, 1);
+      array.splice(...moreNum() - 1, num.length);
     }
   }
 
