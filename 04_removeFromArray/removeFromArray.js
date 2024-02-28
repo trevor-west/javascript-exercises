0ef8d6d597552dea3d448a499cebff3ad2683ee3
Trevor West <trevorwest@duck.com>
2024-02-28 11:50:53 -0600

    Move position of return, debugging

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,11 +2,10 @@ const removeFromArray = function(array, num) {
 
   for (let i = 0; i < array.length; i++){
     if(array[i] === num[i]) {
-      return array.slice(num[i] - 1, num[i]);
+      array.slice(num - 1, num);
     }
+    return array;
   }
-  
-  return array;
 }
 
 // Do not edit below this line
