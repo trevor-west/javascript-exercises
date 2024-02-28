f03e05ff38081b09e6bbe460ce1f14aa191bdf60
Trevor West <trevorwest@duck.com>
2024-02-28 12:59:07 -0600

    Add loop within loop structure and code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function(array, ...num) {
-
+  for (let i = 0; i < array.length; i++) {
+    for (let x = 0; x < num.length; x++) {
+      if (array[i] === num[x]) {
+        array.splice(num[x] - 1, 1)
+      }
+    }
+  }
 }
 
 // Do not edit below this line
