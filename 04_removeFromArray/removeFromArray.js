b38f179c994a373e7b68596ef4a9803542ff0d0d
Trevor West <trevorwest@duck.com>
2024-02-28 12:25:31 -0600

    Change function to rest parameter usage

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,10 @@
-const removeFromArray = function(array, num) {
+const removeFromArray = function(array, ...num) {
 
   for (let i = 0; i < array.length; i++){
     if(array[i] === num) {
       array.splice(num - 1, 1);
+    } else if (array[i] === moreNum[i]) {
+      array.splice(...moreNum() - 1, 1);
     }
   }
 
