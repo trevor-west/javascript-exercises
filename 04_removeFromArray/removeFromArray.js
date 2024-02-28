f49526d4eecc41cd3cfc309293b59b5423d828cb
Trevor West <trevorwest@duck.com>
2024-02-28 12:55:25 -0600

    Delete all code besides variables to restart
    I am doing this because I have realized I need to
    do a loop within a loop, as the issue with previous
    code is that the num[i] value is not properly checking
    against the array[i] value given the different sizes
    of the array lengths and how the if boolean checks work.
    
    Basically, I need to have a for loop cycle through each of the num
    array values and properly check that against every single array value
    before continuing to the next array value.
    
    I think it will be easier to start from scratch (less einstellung).

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,4 @@
 const removeFromArray = function(array, ...num) {
-// if num is 1, num = [1]
-// if num is 1, 2, 3, then num = [1, 2, 3]
-
-  for (let i = 0; i < array.length; i++){
-    if(array[i] === num[i]) {
-      array.splice(num, 1);
-    }
-  }
-  return array;
 
 }
 
