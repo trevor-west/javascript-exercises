b5b62cd25a34aa05a003ce96eaadcff51bd15cc0
Trevor West <trevorwest@duck.com>
2024-02-29 14:41:54 -0600

    Add console debugger (issue is with x for loop)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,9 @@
 const removeFromArray = function(array, ...num) {
-
+  let x = 0;
   for (let i = 0; i < array.length; i++) {
-    for (let x = 0; x < num.length; x++) {
+    for (x = 0; x < num.length; x++) {
       if (array[i] === num[x]) {
+        console.log(`Current x value: ${x}`);
         array.splice(num[x] - 1, 1);
       }
     }
