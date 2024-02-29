2687383b84d9522709960254180685218bb54348
Trevor West <trevorwest@duck.com>
2024-02-29 14:14:13 -0600

    Revert code to single variable functioning state

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,13 @@
 const removeFromArray = function(array, ...num) {
-  let removedArray = array;
-  let originalArray = array;
 
   for (let i = 0; i < array.length; i++) {
     for (let x = 0; x < num.length; x++) {
-      if (originalArray[i] === num[x]) {
-        array.splice(num[x] - 1, 1)
+      if (array[i] === num[x]) {
+        array.splice(num[x] - 1, 1);
       }
     }
   }
-  return removedArray;
+  return array;
 }
 
 // returns single values fine, still cannot handle multi values
