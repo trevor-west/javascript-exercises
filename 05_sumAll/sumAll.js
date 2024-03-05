c5ffa9504a66431e029a204e16a2067a4686ae1f
Trevor West <trevorwest@duck.com>
2024-03-05 09:12:42 -0600

    Add conditionals to satisfy later tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,17 +2,24 @@ const sumAll = function(numOne, numTwo) {
   let arraySum = [];
   let result = 0;
 
-  for (let x = numOne; x <= numTwo; x++){
-    arraySum.push(x);
-  }
+  if (typeof numOne !== 'number' || typeof numTwo !== 'number' || numOne < 0 || numTwo < 0) {
+    return 'ERROR';
+  } else if (numOne > numTwo) {
+      let z = numOne;
+      numOne = numTwo;
+      numTwo = z;
 
-  for (let y = 0; y <= arraySum.length - 1; y++){
-    result += arraySum[y];
-  }
-
-  return result;
-
-};
+      for (let x = numOne; x <= numTwo; x++){
+        arraySum.push(x);
+      }
+    
+      for (let y = 0; y <= arraySum.length - 1; y++){
+        result += arraySum[y];
+      }
+    
+      return result;
+    }
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
