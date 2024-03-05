82aeed0d669a49763d674fc407dd5ed8309b67cf
Trevor West <trevorwest@duck.com>
2024-03-04 20:08:57 -0600

    Add arraySum var and return

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,11 @@
 const sumAll = function(numOne, numTwo) {
+  let arraySum = [];
+
   for (let x = numOne; x < numTwo; x++){
-    
+    arraySum.pop(x);
   }
+
+  return arraySum;
 };
 
 // Do not edit below this line
