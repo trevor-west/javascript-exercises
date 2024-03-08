f9057ef3b06ba5cfecda061fa31a879746ff9e09
Trevor West <trevorwest@duck.com>
2024-03-07 20:26:46 -0600

    Add solution plus credit source

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,8 @@
 const removeFromArray = function(array, ...num) {
-  let x = 0;
-  for (let i = 0; i < array.length; i++) {
-    for (x = 0; x < num.length; x++) {
-      if (array[i] === num[x]) {
-        console.log(`Current x value: ${x}`);
-        array.splice(num[x] - 1, 1);
-      }
-    }
-  }
-  return array;
-}
+  return array.filter( val => !num.includes(val))
+};
 
-// returns single values fine, still cannot handle multi values
-// correctly -- issue should lie in splice(num[x])
+// credit: https://stackoverflow.com/questions/71534020/how-do-i-remove-multiple-elements-from-an-array
 
 // Do not edit below this line
 module.exports = removeFromArray;
