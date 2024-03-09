c6e8ae861b9c96e8d1fb5a8c73dfcafe7f2f4939
Trevor West <trevorwest@duck.com>
2024-03-09 12:32:20 -0600

    Add Math.round and spaces for visibility

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,14 +2,14 @@
 x °F ≘ (x − 32) * (5 / 9)°C
 */
 const convertToCelsius = function(cels) {
-  return ((cels - 32) * (5 / 9));
+  return Math.round( ( ( (cels - 32) * (5 / 9) ) ) * 10) / 10;
 };
 
 /*
 x °C ≘ (x * 9 / 5 + 32)°F
 */
 const convertToFahrenheit = function(fahren) {
-  return (((fahren) * (9 / 5)) + 32);
+  return round( ( ( ( (fahren) * (9 / 5) ) + 32) ) * 10) / 10;
 };
 
 // Do not edit below this line
