2d7089b43891e2c209bd6e764e3c88dd0d063ad7
Trevor West <trevorwest@duck.com>
2024-03-09 12:27:13 -0600

    Add code for conversios, remove .skip

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
 /*
 x °C ≘ (x * 9 / 5 + 32)°F
 */
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  return (((fahren) * (9 / 5)) + 32);
 };
 
 /*
 x °F ≘ (x − 32) * (5 / 9)°C
 */
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  return ((cels - 32) * (5 / 9));
 };
 
 // Do not edit below this line
